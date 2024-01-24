bf317d817015933f99ac972aebf915bb62bb56c9
Lewis <tuannguyenitpy@gmail.com>
2024-01-24 17:11:04 +0700

    Update

--- apps/zeni-ui-components/src/main.tsx
+++ apps/zeni-ui-components/src/main.tsx
@@ -1,10 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { ZeniProvider } from "zeni-ui";
 import App from "./App.tsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <ZeniProvider>
-    <App />
-  </ZeniProvider>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
--- main-app/src/App.tsx
+++ main-app/src/App.tsx
@@ -1,13 +1,17 @@
 import { Suspense, lazy } from "react";
 import "./App.css";
 import ErrorBoundary from "./components/ErrorBoundary";
+import { Radio, RadioGroup, Select, THEMES, useChangeTheme } from "zeni-ui";
 const UIComponentApp = lazy(() => import("zeni_ui_components/all"));
 
 function App() {
+  const { theme, setTheme } = useChangeTheme();
   return (
     <>
       <h1>ZENI GUY</h1>
       <br />
+
+      
 
       <Suspense fallback={<div>Loading...</div>}>
         <ErrorBoundary>

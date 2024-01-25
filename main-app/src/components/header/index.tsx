e8e614ea18f9bb0c200f6e92b9d1ff6166cc1604
Lewis <tuannguyenitpy@gmail.com>
2024-01-25 15:54:03 +0700

    Install yarn workspace

--- main-app/src/components/header/index.tsx
+++ main-app/src/components/header/index.tsx
@@ -1,8 +1,14 @@
+import { GithubLogo, InstagramLogo } from "@phosphor-icons/react";
+
 const Header = () => {
   return (
     <div className="w-full shadow-sm bg-white">
       <div className="px-4 w-full mx-auto max-w-[992px] flex justify-between items-center gap-4 h-[56px]">
         <h1 className="text-xl font-bold">ZENI</h1>
+        <div className="flex items-center gap-6 justify-end">
+          <GithubLogo size={20} weight="bold" />
+          <InstagramLogo size={20} weight="bold" />
+        </div>
       </div>
     </div>
   );

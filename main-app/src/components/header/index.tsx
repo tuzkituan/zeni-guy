b027c85316d3a4fce4fc9e3c72603ccf01362539
Lewis <tuannguyenitpy@gmail.com>
2024-01-25 14:46:33 +0700

    Add Header

---
+++ main-app/src/components/header/index.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="w-full shadow-sm bg-white">
+      <div className="px-4 w-full mx-auto max-w-[992px] flex justify-between items-center gap-4 h-[56px]">
+        <h1 className="text-xl font-bold">ZENI</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- main-app/src/layouts/main-layout.tsx
+++ main-app/src/layouts/main-layout.tsx
@@ -1,8 +1,12 @@
+import Header from "../components/header";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-slate-200">{children}</div>
-  )
-}
+    <div>
+      <Header />
+      <div className="p-4 w-full mx-auto max-w-[992px]">{children}</div>
+    </div>
+  );
+};
 
-export default MainLayout
+export default MainLayout;
